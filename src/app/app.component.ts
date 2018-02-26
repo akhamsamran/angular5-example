9f13cd3fdd0c5a65a3a261eda377b507eb8f5e1d
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-26 16:47:18 -0700

    added app/app.module.ts, app.components.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "angular5-example-app",
+	templateUrl: "./templates/angular5-example-app.html"
+})
+
+export class AppComponent {}
